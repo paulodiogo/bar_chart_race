e1fffde122b15f01680539da6b46a9dd56c6b85b
paulodiogo <opaulodiogo@gmail.com>
2023-10-20 15:04:25 -0300

    ok

--- bar_chart_race/_pyramid_chart_race.py
+++ bar_chart_race/_pyramid_chart_race.py
@@ -320,7 +320,7 @@ class _PyramidChartRace(CommonChart):
 
 
 
-        ax.set_xlim(-10, 10)
+        ax.set_xlim(-.9, .9)
 
     def create_figure(self):
         fig = plt.Figure(**self.fig_kwargs)
